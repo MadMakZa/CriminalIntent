0f560ca425c3916901ce8c9ef34cd9ff73fc7e10
MadMakZa <immakza@gmail.com>
2020-12-15 22:43:48 +0300

    add Edit text method and Button

--- app/src/main/java/com/example/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/example/criminalintent/CrimeFragment.java
@@ -2,9 +2,13 @@ package com.example.criminalintent;
 
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,6 +17,8 @@ import androidx.fragment.app.Fragment;
 public class CrimeFragment extends Fragment {
 
     private Crime mCrime;
+    private EditText mTitleField;
+    private Button mDateButton;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -24,6 +30,29 @@ public class CrimeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_crime, container, false);
+
+        mTitleField = (EditText) v.findViewById(R.id.crime_title);
+        mTitleField.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            //ввод пользователя в char приводим к string
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                mCrime.setTitle(s.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        //настройка текста кнопки
+        mDateButton = (Button) v.findViewById(R.id.crime_date);
+        mDateButton.setText(mCrime.getDate().toString());
+        mDateButton.setEnabled(false);
         return v;
     }
 }
