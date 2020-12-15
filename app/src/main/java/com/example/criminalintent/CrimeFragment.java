04de2a1de4053fc131b9497b21aadfff8ffb3979
MadMakZa <immakza@gmail.com>
2020-12-15 22:38:09 +0300

    class crime fragment

---
+++ app/src/main/java/com/example/criminalintent/CrimeFragment.java
@@ -0,0 +1,29 @@
+package com.example.criminalintent;
+
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class CrimeFragment extends Fragment {
+
+    private Crime mCrime;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mCrime = new Crime();
+    }
+
+    //явное заполнение фрагмента
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_crime, container, false);
+        return v;
+    }
+}
