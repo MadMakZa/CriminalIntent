62e70d4026f67317640081caca76752d1b6bb2cf
MadMakZa <immakza@gmail.com>
2020-12-17 23:30:47 +0300

    add singlton CrimeLab newList

--- app/src/main/java/com/example/criminalintent/CrimeLab.java
+++ app/src/main/java/com/example/criminalintent/CrimeLab.java
@@ -2,11 +2,16 @@ package com.example.criminalintent;
 
 import android.content.Context;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
 /**
  * Синглтон класс
  */
 public class CrimeLab {
     private static CrimeLab sCrimeLab;
+    private List<Crime> mCrimes;
 
     public static CrimeLab get(Context context){
         if (sCrimeLab == null){
@@ -15,6 +20,18 @@ public class CrimeLab {
         return sCrimeLab;
     }
     private CrimeLab(Context context){
-
+        mCrimes = new ArrayList<>();
     }
+    public List<Crime> getCrimes(){
+        return mCrimes;
+    }
+    public Crime getCrime(UUID id) {
+        for (Crime crime : mCrimes) {
+            if (crime.getId().equals(id)) {
+                return crime;
+            }
+        }
+        return null;
+    }
+
 }
